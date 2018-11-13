04cd68c74ccb02675e03b7dc1b3f43146218a489
PedroHenriqueSouza <pedrosouzaferreira3@gmail.com>
2018-11-13 15:00:46 -0200

    15° Commit-Criação da pagina Membros do Conselho

---
+++ membrosconselho.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" media="screen" href="menu.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="membrosconselho.css" />
+    <script src="main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+<body >
+    <?php
+        require_once("Head.php");
+           
+    ?>
+
+<div class="container">
+<!--Primeiro Card-->
+<div class="row mb-5">
+            
+        <div class="col-md-4 mt-3">
+              
+            <div class="card">             
+                            
+                <div class="card-body">
+                  
+                <a href="http://lattes.cnpq.br/9843838056118017" 	target='_blank'> <img class="quadro mb-4" /></a>
+                  <h4 class="card-title" style="min-height: 55px;">Titular: Dr. GUSTAVO HENRIQUE GRAVATIM COSTA – Coordenador</h4>
+                  <h6 class="card-subtitle mb-3 text-muted">Suplente: Dra. VanescaKorasaki – Vice Coordenadora </h6>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+<?php
+        require_once("footer.php");
+    ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+    
