1df979a89c58788e0b1a414db331ee76556abe9d
Fernando <flsoncin@globo.com>
2018-11-21 14:31:05 -0200

    27º commit - Linhas de Pesquisa

---
+++ pesquisa.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" media="screen" href="menu.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="docente.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="footer.css" />
+    <script src="main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+
+</head>
+<body >
+    <?php
+        require_once("Head.php");
+    ?>
+
+    <div class="container" style="padding-left:5%;padding-right:5%;text-align: justify">
+            <div class="row" >
+                <h2> LINHAS DE PESQUISA </h2>
+            </div>
+            <br>
+            <div class="row">
+            
+                <li>Diagnóstico e Ecologia Ambiental</li>
+                <br><br>
+                <p> Os projetos desenvolvidos nesta linha de pesquisa visam o monitoramento ambiental para o levantamento dos aspectos ecológicos, geográficos e hídricos, 
+                promovendo a compreensão e ampliação dos conhecimentos sobre a biodiversidade, os ecossistemas e a ação das atividades antrópicas sobre os recursos naturais;
+                contribuindo para tomada de decisões sobre o uso racional, manejo e conservação dos recursos naturais.</p>
+
+                <li>Tecnologia, Ambiente e Sociedade</li>
+                <br><br>
+                <p> Os estudos desenvolvidos nessa linha contribuirão para o desenvolvimento de tecnologias inovadoras que minimizem os impactos gerados pela ação antrópica no 
+                meio ambiente, a partir do desenvolvimento de métodos e materiais para o controle ambiental e produção sustentável. Também será trabalhado a reutilização de resíduos 
+                agroindustriais, químicos e minerais, ênfase na bioenergia, produção de alimentos e educação ambiental; objetivando o bem estar e a qualidade de vida da sociedade.</p>
+
+
+
+            </div>
+        </div>
+
+
+
+
+        <?php
+        require_once("footer.php");
+    ?>
+</body>
+</html>
